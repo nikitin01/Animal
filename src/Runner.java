b3bd983f59384405696c904ac9acc9c43b0dc910
nikitin01 <nikitin01@mail.ru>
2020-01-20 12:39:34 +0100

    Added Cat class
    Created cat object in Runner

--- src/Runner.java
+++ src/Runner.java
@@ -3,5 +3,7 @@ public class Runner {
     public static void main(String[] args) {
         Dog dog = new Dog();
         dog.makeNoise();
+        Cat cat = new Cat();
+        cat.makeNoise();
     }
 }
